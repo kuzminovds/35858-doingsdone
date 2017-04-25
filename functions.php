dc8fac6e3b3201d9c2c0e2153eab79304b0ad16f
Dmitry Kuzminov <kuzminovds@gmail.com>
2017-04-25 23:36:35 +0300

    Шаблонизация проекта v1

--- functions.php
+++ functions.php
@@ -6,10 +6,12 @@
  */
 function includeTemplate($filename, $data) {
 	if ('templates/'.str_replace('..','',$filename).'.php') {
-			ob_start();
 		htmlspecialchars($data);
+		ob_start();
 		include 'templates/'.$filename.'.php';
-		return ob_get_clean();
+		$text = ob_get_contents();
+		ob_end_clean();
+		return $text;
 		
 	} else {
 		return '';
