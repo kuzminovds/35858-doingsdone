37bad6cc20ebf7bc24a350dadf65d9d2b22ce019
Dmitry Kuzminov <kuzminovds@gmail.com>
2017-05-01 12:20:55 +0300

    #13 Получение данных из форм 1-7

--- templates/form.php
+++ templates/form.php
@@ -3,12 +3,12 @@
 
     <h2 class="modal__heading">Добавление задачи</h2>
 
-    <form class="form" class="" action="index.php" method="post">
+    <form class="form" class="" action="index.php?add" method="post">
       <div class="form__row">
         <label class="form__label" for="name">Название <sup>*</sup></label>
         <?php if(isset($data['errors']['name'])): ?>
           <input class="form__input form__input--error" type="text" name="name" id="name" value="" placeholder="Введите название">
-          <span class="form__error"><?=$data['errors']['name']; ?></span>
+          <span class="form__error error-massage"><?=$data['errors']['name']; ?></span>
         <?php else: ?>
           <input class="form__input" type="text" name="name" id="name" value="<?=$data['last_data']['name']; ?>" placeholder="Введите название">
         <?php endif; ?>
@@ -16,15 +16,28 @@
 
       <div class="form__row">
         <label class="form__label" for="project">Проект <sup>*</sup></label>
-        <select class="form__input form__input--select" name="project" id="project">
-          <option value="">Выбрать проект...</option>
-          <option value="Входящие">Входящие</option>
-        </select>
+        <?php if (isset($data['errors']['project'])): ?>
+          <select class="form__input form__input--select form__input--error" name="project" id="project">
+            <option value="">Выбрать проект...</option>
+            <option value="Входящие">Входящие</option>
+          </select>
+          <span class="form__error error-massage"><?=$data['errors']['project']; ?></span>
+        <?php else: ?>
+          <select class="form__input form__input--select" name="project" id="project">
+            <option value="">Выбрать проект...</option>
+            <option value="Входящие" <?php @$data['last_data']['project']=="Входящие" ? print "selected" : false;?>>Входящие</option>
+          </select>
+        <?php endif ?>
       </div>
 
       <div class="form__row">
         <label class="form__label" for="date">Дата выполнения <sup>*</sup></label>
-        <input class="form__input form__input--date" type="text" name="date" id="date" value="<?=$_POST["date"]; ?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+        <?php if (isset($data['errors']['date'])): ?>
+          <input class="form__input form__input--date form__input--error" type="text" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+          <span class="form__error error-massage"><?=$data['errors']['date']; ?></span>
+        <?php else: ?>
+          <input class="form__input form__input--date" type="text" name="date" id="date" value="<?=$data['last_data']['date']; ?>" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+        <?php endif ?>
       </div>
 
       <div class="form__row">
