aacfc009c798afb0131cd1c76fb69a925f087fd8
Dmitry Kuzminov <kuzminovds@gmail.com>
2017-05-04 23:39:57 +0300

    #18 Пишем SQL-запросы - готово

--- queries.sql
+++ queries.sql
@@ -4,15 +4,15 @@ SELECT * FROM projects;
 -- получить список из всех задач для одного проекта;
 SELECT * FROM tasks WHERE project_id = 1;
 
--- пометить задачу как выполненную; -- ??? тут не понял, прописать условие if ??? 
--- ??? или достаточно просто присвоить дату выполнения задания ???
-INSERT INTO tasks SET name = 'Встреча с другом', dt_ready = '2017.05.05';
+-- пометить задачу как выполненную;
+INSERT INTO tasks SET title = 'Встреча с другом', deadline = '2017.05.05', project_id = 1; -- создаем задачу
+UPDATE tasks SET dt_ready = '2017.05.05'; WHERE id = 1 AND title = 'Встреча с другом';     -- помечаем задачу
 
 -- добавить новый проект;
 INSERT INTO projects SET name = 'Сайт';
 
 -- добавить новую задачу (включает указание проекта, дату завершения, название);
-INSERT INTO tasks SET title = 'Встреча с другом', deadline = '05.05.2017', project_id = 1;
+INSERT INTO tasks SET title = 'Встреча с другом', deadline = '2017.05.05', project_id = 1;
 
 -- получить все задачи для завтрашнего дня;
 SELECT * FROM tasks WHERE deadline = '2017.05.05';
